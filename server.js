f8394b5cdd56b0747c3c9e8d86a5ff1780f00fe9
Gabriel Jimenez <201839235@edu.clg.qc.ca>
2024-08-27 14:12:24 -0400

    Ajout de commentaires

--- server.js
+++ server.js
@@ -9,3 +9,4 @@ let reqInfo = {url:req.url, method:req.method};
 });
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+//Mon premier server http
